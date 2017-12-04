2394ed41bdabb8df96b6f422134d8afbcae73bf9
Josh Duff <me@JoshDuff.com>
2017-12-04 14:10:32 -0600

    Make sermon text/audio updater work off of relative paths
    Also yay autoformatting

--- sermon-text-and-audio-correlater/index.js
+++ sermon-text-and-audio-correlater/index.js
@@ -1,35 +1,37 @@
-require('reify')
+require(`reify`)
 
-require('loud-rejection')()
+require(`loud-rejection`)()
 
-const Butler = require('noddity-butler')
-const level = require('level-mem')
-const denodeify = require('then-denodeify')
-const Retrieval = require('noddity-fs-retrieval')
-const sh = require('shell-tag')
+const path = require(`path`)
 
-const compareDateAsc = require('date-fns/compare_asc')
-const formatDate = require('date-fns/format')
-const isSunday = require('date-fns/is_sunday')
-const addDays = require('date-fns/add_days')
+const Butler = require(`noddity-butler`)
+const level = require(`level-mem`)
+const denodeify = require(`then-denodeify`)
+const Retrieval = require(`noddity-fs-retrieval`)
+const sh = require(`shell-tag`)
 
-const getSermonAudioData = require('./sermon-rss')
-const passageToRange = require('./passage-to-range')
-const createPodcastXmlFile = require('./compile-podcast')
+const compareDateAsc = require(`date-fns/compare_asc`)
+const formatDate = require(`date-fns/format`)
+const isSunday = require(`date-fns/is_sunday`)
+const addDays = require(`date-fns/add_days`)
 
-const guaranteeRange = require('../client/lib/structure/guarantee-range').default
+const getSermonAudioData = require(`./sermon-rss`)
+const passageToRange = require(`./passage-to-range`)
+const createPodcastXmlFile = require(`./compile-podcast`)
+
+const guaranteeRange = require(`../client/lib/structure/guarantee-range`).default
 
 sh`
-cd /Users/josh/code/KayserCommentary
+cd ${ path.join(__dirname, `../../KayserCommentary`) }
 git fetch upstream
 git checkout upstream/master
 rm -rf content
 node ci/deploy/bin.js Markdown/Web/ ./content
 `
 
-const retrieval = new Retrieval('/Users/josh/code/KayserCommentary/content')
+const retrieval = new Retrieval(path.join(__dirname, `../../KayserCommentary/content`))
 
-const butler = Butler(retrieval, level('server'), {
+const butler = Butler(retrieval, level(`server`), {
 	parallelPostRequests: 10,
 })
 
@@ -47,12 +49,10 @@ async function main() {
 		return map
 	}, Object.create(null))
 
-	const revelationPosts = posts.filter(post => {
-		return post.metadata.published
+	const revelationPosts = posts.filter(post => post.metadata.published
 			&& /^Sermons\/New Testament\/Revelation\/Revelation/.test(post.filename)
 			&& !/GraphicsCharts/.test(post.filename)
-			&& !/Revelation timeline/.test(post.filename)
-	}).map(({ filename, metadata }) => {
+			&& !/Revelation timeline/.test(post.filename)).map(({ filename, metadata }) => {
 		const { passage, date, title } = metadata
 		return {
 			title,
@@ -60,27 +60,25 @@ async function main() {
 			filename,
 			date,
 		}
-	}).sort((postA, postB) => {
-		return compareDateAsc(postA.date, postB.date)
-	})
+	}).sort((postA, postB) => compareDateAsc(postA.date, postB.date))
 
 	// print(revelationPosts)
 
 	const audioIdsSeenAlready = new Set()
 
 	const postsAndAudio = revelationPosts.map(({ filename, passage, date, title }) => {
-		const isoDateString = formatDate(nextSunday(date), 'YYYY-MM-DD')
+		const isoDateString = formatDate(nextSunday(date), `YYYY-MM-DD`)
 		// console.log(isoDateString, title)
 		const audioId = dateToIdAndTitle[isoDateString] && dateToIdAndTitle[isoDateString].id
 
 		if (audioId) {
 			if (audioIdsSeenAlready.has(audioId)) {
-				throw new Error(`Can't add sermon audioId ${audioId} to the sermon list twice!, ${isoDateString}`)
+				throw new Error(`Can't add sermon audioId ${ audioId } to the sermon list twice!, ${ isoDateString }`)
 			}
 			audioIdsSeenAlready.add(audioId)
 		}
 
-		const range = isoDateString === '2015-04-26' ? [ [ 21, 1 ], [ 21, 1 ] ] : passageToRange(passage)
+		const range = isoDateString === `2015-04-26` ? [ [ 21, 1 ], [ 21, 1 ] ] : passageToRange(passage)
 		return {
 			title,
 			passage,
@@ -112,10 +110,8 @@ function trimToPropertiesThatNeedToBeDownloadedToClient(structure) {
 const rangeRegex = /"range": \[\s*\[\s*(\d+),\s*(\d+),\s*(\d+)\s*\],\s*\[\s*(\d+),\s*(\d+),\s*(\d+)\s*\]\s*\]/mg
 
 function toJson(structure) {
-	const json = JSON.stringify(structure, null, '\t')
-	return json.replace(rangeRegex, (match, startChapter, startVerse, startSection, endChapter, endVerse, endSection) => {
-		return `"range": [[${startChapter},${startVerse},${startSection}], [${endChapter},${endVerse},${endSection}]]`
-	})
+	const json = JSON.stringify(structure, null, `\t`)
+	return json.replace(rangeRegex, (match, startChapter, startVerse, startSection, endChapter, endVerse, endSection) => `"range": [[${ startChapter },${ startVerse },${ startSection }], [${ endChapter },${ endVerse },${ endSection }]]`)
 }
 
 function print(structure) {
@@ -124,15 +120,13 @@ function print(structure) {
 
 function save(structure) {
 	const json = toJson(structure)
-	require('fs').writeFileSync('../public/static/sermons.json', json)
+	require(`fs`).writeFileSync(`../public/static/sermons.json`, json)
 }
 
 function guaranteeRangeSections(sermons) {
-	return sermons.map(sermon => {
-		return Object.assign({}, sermon, {
-			range: guaranteeRange([ sermon.range[0], sermon.range[1] ]),
-		})
-	})
+	return sermons.map(sermon => Object.assign({}, sermon, {
+		range: guaranteeRange([ sermon.range[0], sermon.range[1] ]),
+	}))
 }
 
 function nextSunday(date) {
