11ed591cc159bc385c4c82ad7c885284de8a046a
Josh Duff <me@JoshDuff.com>
2018-01-09 15:50:06 -0600

    Preprocessing Svelte components with PostCSS

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,40 @@ import babel from 'rollup-plugin-babel'
 import svelte from 'rollup-plugin-svelte'
 import json from 'rollup-plugin-json'
 import visualizer from 'rollup-plugin-visualizer'
+import sveltePreprocessPostcss from 'svelte-preprocess-postcss'
+
+import precss from 'precss'
+import autoprefixer from 'autoprefixer'
+
+const reasonablePrecss = precss({
+	import: {
+		path: [ `client/global-css` ],
+		prefix: ``,
+	},
+})
+
+
 
 export default {
-	name: 'revelationStructure',
-	input: './client/index.js',
+	name: `revelationStructure`,
+	input: `./client/index.js`,
 	output: {
-		file: './public/static/index-bundle.js',
-		format: 'iife',
+		file: `./public/static/index-bundle.js`,
+		format: `iife`,
 	},
 	sourcemap: true,
 	plugins: [
-		svelte(),
+		svelte({
+			preprocess: {
+				style: sveltePreprocessPostcss({
+					useConfigFile: false,
+					plugins: [ reasonablePrecss, autoprefixer ],
+				}),
+			},
+			css(css) {
+				css.write(`public/static/components.css`)
+			},
+		}),
 		commonjs(),
 		json(),
 		resolve({
@@ -24,14 +47,14 @@ export default {
 			babelrc: false,
 			presets: [
 				[
-					'es2015',
+					`es2015`,
 					{
 						modules: false,
 					},
 				],
 			],
 			plugins: [
-				'external-helpers',
+				`external-helpers`,
 			],
 		}),
 		visualizer(),
