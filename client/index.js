6da894872ea47648fa5d5b26410939df7c730045
Josh Duff <me@JoshDuff.com>
2018-01-24 17:01:03 -0600

    Fix development/production flag not being read

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 console.log(process.env.NODE_ENV)
-// const TEST = process.env.NODE_ENV !== `production`
-const TEST = true
+const TEST = process.env.NODE_ENV !== `production`
 
 Raven.config(`https://abebea60be4f48689cd1d3c7684c93cd@sentry.io/270211`, {
 	environment: TEST ? `client:development` : `client:production`,
