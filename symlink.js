a4abd0426daaf852440617e30c2adcedf397b41b
Josh Duff <me@JoshDuff.com>
2018-02-21 11:25:21 -0600

    Use symlink-cli

--- symlink.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const { symlinkSync } = require('fs')
-
-symlinkSync(process.argv[2], process.argv[3], 'dir')
-
-console.log('Symlinked', process.argv[2], 'to', process.argv[3])
